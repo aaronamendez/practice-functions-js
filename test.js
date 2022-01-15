1dd7212ec89e669d00a0311eae6ecaa4e07d9161
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-01-15 17:19:25 -0500

    First commit.

---
+++ test.js
@@ -0,0 +1,67 @@
+// Order of Ranks eg: Iron < Bronze < Silver < Gold < Platinum < Diamond
+const ranks = ['iron', 'bronze', 'silver', 'gold', 'platinum', 'diamond'];
+const players = [
+	{
+		id: 1,
+		name: 'John',
+		rank: 'platinum',
+	},
+	{
+		id: 1,
+		name: 'Timmy',
+		rank: 'silver',
+	},
+	{
+		id: 1,
+		name: 'Aaron',
+		rank: 'diamond',
+	},
+	{
+		id: 1,
+		name: 'Marcos',
+		rank: 'iron',
+	},
+	{
+		id: 1,
+		name: 'Kenneth',
+		rank: 'bronze',
+	},
+	{
+		id: 1,
+		name: 'Wilson',
+		rank: 'gold',
+	},
+	{
+		id: 1,
+		name: 'Sparky',
+		rank: 'gold',
+	},
+	{
+		id: 1,
+		name: 'Sam',
+		rank: 'silver',
+	},
+	{
+		id: 1,
+		name: 'Allison',
+		rank: 'diamond',
+	},
+	{
+		id: 1,
+		name: 'Randy',
+		rank: 'bronze',
+	},
+	{
+		id: 1,
+		name: 'Stephanie',
+		rank: 'iron',
+	},
+];
+
+// Create a function named findRanksByName that:
+// 1. Receives an Array, and a rank.
+// 2. Return a new Array that in that holds the name of ALL the players that hold that rank.
+
+// Create a function named setNewPlayer that:
+// 1. Receives an Array.
+// 2. Returns the array with the newly added player holding a 'name' and a 'rank'
